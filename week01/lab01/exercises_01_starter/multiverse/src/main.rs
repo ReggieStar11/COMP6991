07446c2a00bf88e145deba87c5e142acef54f41c
Richard Peng <richardpeng914@gmail.com>
2025-09-19 18:45:49 +1000

    completed multiverse

--- week01/lab01/exercises_01_starter/multiverse/src/main.rs
+++ week01/lab01/exercises_01_starter/multiverse/src/main.rs
@@ -1,35 +1,45 @@
 // hmm this doesn't look right!!
-struct universe_details {
-    String universe_name;
-    String universe_winner;
-    int universe_population;
+struct UniverseDetails {
+    universe_name: String,
+    universe_winner: String,
+    universe_population: u32,
 }
 
-fn get_universe_details(universe_id: u32) -> Option<universe_details> {
-    // does this even compile??
-    struct universe_details;
-    if universe_id % 3 == 0 {
-        return None(universe_details {
-            universe_name: "Milky Way",
-            universe_winner: "The Galaxy".to_string(),
-            universe_population: "six",
-        })   
-    } 
+fn get_universe_details(universe_id: u32) -> Option<UniverseDetails> {
+    if universe_id % 3 == 0 && universe_id % 5 == 0 {
+        return Some(UniverseDetails {
+            universe_name: "Stardew Valley".to_string(),
+            universe_winner: "Jojo Corp".to_string(),
+            universe_population: 1,
+        });
+    } else if universe_id % 3 == 0 {
+        return Some(UniverseDetails {
+            universe_name: "Star Wars".to_string(),
+            universe_winner: "The Rebellion".to_string(),
+            universe_population: 4294967295,
+        });
+    } else if universe_id % 5 == 0 {
+        return Some(UniverseDetails {
+            universe_name: "Miraculous".to_string(),
+            universe_winner: "Hawk Moth".to_string(),
+            universe_population: 22,
+        });
+    }
+    None
 }
-
+// No changes needed here; "Star Wars" is already a string literal, but Rust requires .to_string() to convert it to a String type.
 
 // this main function is fine, except for two gaps
-// the print statements could make use of "{variable}" instead of 
+// the print statements could make use of "{variable}" instead of
 // ("{}", variable)
+
 fn main() {
     for id in 1..=15 {
         let universe_details = get_universe_details(id);
-        if let Some(/* FILL ME IN */) = /* FILL ME IN */ {
-            println!("Universe with id {} is called {}, won by {} and has a population of {}", 
-                id, 
-                details.universe_name, 
-                details.universe_winner, 
-                details.universe_population
+        if let Some(d) = universe_details {
+            println!(
+                "Universe with id {} is called {}, won by {} and has a population of {}",
+                id, d.universe_name, d.universe_winner, d.universe_population
             );
         } else {
             println!("Universe with id {} is unknown", id);
---
+++ week01/lab01/exercises_01_starter/multiverse/target/debug/deps/multiverse-91855b2b9113c0bc.d
@@ -0,0 +1,5 @@
+/Users/richard/Coding/COMP6991/week01/lab01/exercises_01_starter/multiverse/target/debug/deps/multiverse-91855b2b9113c0bc.d: src/main.rs
+
+/Users/richard/Coding/COMP6991/week01/lab01/exercises_01_starter/multiverse/target/debug/deps/multiverse-91855b2b9113c0bc: src/main.rs
+
+src/main.rs:
