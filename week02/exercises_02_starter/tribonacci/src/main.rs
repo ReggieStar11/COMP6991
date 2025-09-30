09f78adeb0814d20bbd1213743884a57f71cfa7a
Richard Peng <richardpeng914@gmail.com>
2025-09-30 15:26:46 +1000

    fixed trib

--- week02/exercises_02_starter/tribonacci/src/main.rs
+++ week02/exercises_02_starter/tribonacci/src/main.rs
@@ -5,6 +5,7 @@ struct TribonacciError(String);
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    
     let error_message = String::from("Please enter a valid size");
 
     let size = match args.get(1) {
@@ -15,6 +16,8 @@ fn main() {
     if let Err(e) = compute_tribonacci(size, error_message) {
         println!("Error: {}", e.0)
     }
+
+    
 }
 
 /// Computes the tribonacci sequence of a given size
@@ -25,6 +28,39 @@ fn compute_tribonacci(
     // inside the `TribonacciError` struct
     error_msg: String,
 ) -> Result<(), TribonacciError> {
+    
+    
+    let target_size = match size {
+        Ok(n) => n,
+        Err(_) => return Err(TribonacciError(error_msg)),  
+    };
+   
+    // let target_size= size.unwrap();
+
+    if target_size > 145 {
+        return Err(TribonacciError(error_msg));
+    }
+
+        
+    let mut numbers: Vec<i128> = vec![1,1,1];
+    let mut num_size = numbers.len();
+
+    while num_size < target_size {
+        let first = numbers[num_size-1];
+        let second = numbers[num_size-2];
+        let third = numbers[num_size-3];
+        
+        let sum = first + second + third;
+        
+        // println!("{}", sum);
+        numbers.push(sum);
+        num_size = numbers.len();
+    }
+    
+    
+    // println!("{:?}", numbers);
     // TODO: complete this function!
+    println!("Values: {:?}", numbers);
     Ok(())
+
 }
---
+++ week02/exercises_02_starter/tribonacci/target/debug/tribonacci.d
@@ -0,0 +1,1 @@
+/Users/richard/Coding/COMP6991/week02/exercises_02_starter/tribonacci/target/debug/tribonacci: /Users/richard/Coding/COMP6991/week02/exercises_02_starter/tribonacci/src/main.rs
