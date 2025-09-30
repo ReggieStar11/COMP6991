e4a6d06219c39c99f46109bf0d706e3e21686b53
Richard Peng <richardpeng914@gmail.com>
2025-09-30 22:01:16 +1000

    few changes

--- week02/exercises_02_starter/data_analysis/src/main.rs
+++ week02/exercises_02_starter/data_analysis/src/main.rs
@@ -19,8 +19,8 @@ struct Data {
 }
 fn main() {
     let mut rdr = ReaderBuilder::new()
-        .delimiter(b'|') // Set delimiter to pipe character for PSV
-        .has_headers(false) // Your data doesn't have headers
+        .delimiter(b'|') 
+        .has_headers(false) 
         .from_path(ENROLMENTS_PATH)
         .expect("Failed to open file");
 
@@ -44,15 +44,15 @@ fn main() {
     let mut common = String::new();
     let mut uncommon = String::new();
     let mut common_num: usize = 0;
-    let mut uncommon_num: usize = usize::MAX;  // Start with max value
+    let mut uncommon_num: usize = usize::MAX;  
 
     for (course, students) in &grouped {
-        if students.len() > common_num.try_into().unwrap() {
+        if students.len() > common_num {
             common_num = students.len();
             common = course.clone();
         }
 
-        if students.len() < uncommon_num.try_into().unwrap() {
+        if students.len() < uncommon_num {
             uncommon_num = students.len();
             uncommon = course.clone();
         }
@@ -78,7 +78,7 @@ fn group_by_course(records: &[Data]) -> HashMap<String, Vec<&Data>> {
         hashmap
             .entry(item.course_code.clone())
             .or_insert_with(Vec::new)
-            .push(item); // Push the reference
+            .push(item);
     }
 
     hashmap
