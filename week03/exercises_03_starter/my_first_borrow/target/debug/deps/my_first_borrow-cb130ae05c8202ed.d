225c9ea7655e4cb5945be28731c33ba68f083e40
Richard Peng <richardpeng914@gmail.com>
2025-10-02 16:05:12 +1000

    finished first

---
+++ week03/exercises_03_starter/my_first_borrow/target/debug/deps/my_first_borrow-cb130ae05c8202ed.d
@@ -0,0 +1,5 @@
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/my_first_borrow/target/debug/deps/my_first_borrow-cb130ae05c8202ed.d: src/main.rs
+
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/my_first_borrow/target/debug/deps/my_first_borrow-cb130ae05c8202ed: src/main.rs
+
+src/main.rs:
---
+++ week03/exercises_03_starter/my_first_borrow/target/debug/my_first_borrow.d
@@ -0,0 +1,1 @@
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/my_first_borrow/target/debug/my_first_borrow: /Users/richard/Coding/COMP6991/week03/exercises_03_starter/my_first_borrow/src/main.rs
