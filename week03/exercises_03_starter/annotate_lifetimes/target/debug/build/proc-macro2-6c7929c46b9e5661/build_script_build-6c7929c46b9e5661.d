c8bf4cb108b731c529c4db26af730ff049df0cb2
Richard Peng <richardpeng914@gmail.com>
2025-10-02 16:38:36 +1000

    more

---
+++ week03/exercises_03_starter/annotate_lifetimes/target/debug/build/proc-macro2-6c7929c46b9e5661/build_script_build-6c7929c46b9e5661.d
@@ -0,0 +1,5 @@
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/annotate_lifetimes/target/debug/build/proc-macro2-6c7929c46b9e5661/build_script_build-6c7929c46b9e5661.d: /Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.101/build.rs
+
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/annotate_lifetimes/target/debug/build/proc-macro2-6c7929c46b9e5661/build_script_build-6c7929c46b9e5661: /Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.101/build.rs
+
+/Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.101/build.rs:
---
+++ week03/exercises_03_starter/annotate_lifetimes/target/debug/deps/annotate_lifetimes-2e2dc2e455114d20.d
@@ -0,0 +1,5 @@
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/annotate_lifetimes/target/debug/deps/annotate_lifetimes-2e2dc2e455114d20.d: src/lib.rs
+
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/annotate_lifetimes/target/debug/deps/libannotate_lifetimes-2e2dc2e455114d20.rmeta: src/lib.rs
+
+src/lib.rs:
