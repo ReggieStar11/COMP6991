a7fc2b71dab23f43af5ffadea7b5b8eaaa4e03ed
Richard Peng <richardpeng914@gmail.com>
2025-10-02 16:19:42 +1000

    fixed

--- week03/exercises_03_starter/pusheen/src/main.rs
+++ week03/exercises_03_starter/pusheen/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
     let mut vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    let a = &mut vec;
+    // Since a is &mut vec, only one can exsit at any given time
+    // since the liftetime of a does not exist before b is called
+    // the code will not be able to comple as vec is being borrowed more than once at a time
+  
+    let a = &mut vec.clone();
     let b = &mut vec;
 
+    // So now with clone a is mutuable reference to the cloned vector,
+    // whilst b is a referenced to the originla vector, meaning the original vector is not existing  twice
+    
     a.push(11);
     b.push(12);
 }
---
+++ week03/exercises_03_starter/pusheen/target/debug/pusheen.d
@@ -0,0 +1,1 @@
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/pusheen/target/debug/pusheen: /Users/richard/Coding/COMP6991/week03/exercises_03_starter/pusheen/src/main.rs
