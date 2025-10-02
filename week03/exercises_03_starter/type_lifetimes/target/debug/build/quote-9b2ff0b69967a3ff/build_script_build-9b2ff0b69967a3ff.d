a55f7ce3c7d8b95a332d228377a28f2d6e65a157
Richard Peng <richardpeng914@gmail.com>
2025-10-02 16:38:12 +1000

    changes

---
+++ week03/exercises_03_starter/type_lifetimes/target/debug/build/quote-9b2ff0b69967a3ff/build_script_build-9b2ff0b69967a3ff.d
@@ -0,0 +1,5 @@
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/type_lifetimes/target/debug/build/quote-9b2ff0b69967a3ff/build_script_build-9b2ff0b69967a3ff.d: /Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.41/build.rs
+
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/type_lifetimes/target/debug/build/quote-9b2ff0b69967a3ff/build_script_build-9b2ff0b69967a3ff: /Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.41/build.rs
+
+/Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.41/build.rs:
---
+++ week03/exercises_03_starter/type_lifetimes/target/debug/build/syn-df2f472cdc821474/build_script_build-df2f472cdc821474.d
@@ -0,0 +1,5 @@
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/type_lifetimes/target/debug/build/syn-df2f472cdc821474/build_script_build-df2f472cdc821474.d: /Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/syn-1.0.109/build.rs
+
+/Users/richard/Coding/COMP6991/week03/exercises_03_starter/type_lifetimes/target/debug/build/syn-df2f472cdc821474/build_script_build-df2f472cdc821474: /Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/syn-1.0.109/build.rs
+
+/Users/richard/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/syn-1.0.109/build.rs:
